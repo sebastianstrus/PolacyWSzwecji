b0f0ea897d495ad90775b1f076025a02ace448a8
Sebastian Strus <sebastian.strus1@gmail.com>
2019-06-01 00:17:00 +0200

    Initial Commit

---
+++ Polacy w Szwecji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Polacy w Szwecji
+//
+//  Created by Sebastian Strus on 2019-06-01.
+//  Copyright © 2019 Sebastian Strus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
