0ad0f86f950f467ed1a560457489f45e2e901de8
Sebastian Strus <sebastian.strus1@gmail.com>
2019-06-15 23:54:14 +0200

    Added search function

--- Polacy w Szwecji/Controller/Main/UsersTVC.swift
+++ Polacy w Szwecji/Controller/Main/UsersTVC.swift
@@ -12,10 +12,11 @@ protocol SideMenuDelegate {
     func shouldToggleMenu()
 }
 
-class UsersTVC: UITableViewController, UISearchControllerDelegate, UISearchBarDelegate {
+class UsersTVC: UITableViewController, UISearchControllerDelegate, UISearchBarDelegate, UISearchResultsUpdating {
     
     var users: [User] = []
     var searchController: UISearchController = UISearchController(searchResultsController: nil)
+    var searchResults: [User] = []
     var sideMenuDelegate:SideMenuDelegate?
     
     
@@ -28,7 +29,48 @@ class UsersTVC: UITableViewController, UISearchControllerDelegate, UISearchBarDe
         setupTableView()
     }
     
-    func setupSearchBarController() {
+    // MARK: - UISearchResultsUpdating methods
+    func updateSearchResults(for searchController: UISearchController) {
+        if  searchController.searchBar.text == nil || searchController.searchBar.text!.isEmpty {
+            view.endEditing(true)
+        } else {
+            let textLowercased = searchController.searchBar.text!.lowercased()
+            filterContent(for: textLowercased)
+        }
+        tableView.reloadData()
+    }
+    
+    func filterContent(for searchText: String) {
+        searchResults = self.users.filter {
+            return $0.username.lowercased().range(of: searchText) != nil
+        }
+    }
+    
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (searchController.isActive ? searchResults : users).count
+        
+    }
+    
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: IDENTIFIER_CELL_USERS, for: indexPath) as! UserCell
+        cell.loadData((searchController.isActive ? searchResults : users)[indexPath.row])
+        return cell
+     }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 94
+    }
+    
+    // MARK: - Private methods
+    @objc private func requestToggleMenu() {
+        sideMenuDelegate?.shouldToggleMenu()
+    }
+    
+    private func setupSearchBarController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        //searchController.dimsBackgroundDuringPresentation = false
         let searchBar = searchController.searchBar
         searchBar.placeholder = "Search users..."
         searchBar.tintColor = UIColor.white
@@ -45,12 +87,12 @@ class UsersTVC: UITableViewController, UISearchControllerDelegate, UISearchBarDe
         navigationItem.searchController = searchController
     }
     
-    func setupNavigationBar() {
+    private func setupNavigationBar() {
         navigationItem.title = "People"
         navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.barTintColor = UIColor.lightRed
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
-    
+        
         let menuBtn = UIBarButtonItem(image: UIImage(named: "menu_icon")?.withRenderingMode(.alwaysTemplate), style: .plain, target: self, action: #selector(requestToggleMenu))
         menuBtn.tintColor = UIColor.white
         navigationItem.leftBarButtonItem = menuBtn
@@ -59,37 +101,16 @@ class UsersTVC: UITableViewController, UISearchControllerDelegate, UISearchBarDe
     @objc private func handleLogout() {
         Api.User.logOut()
     }
-
-    func observeUsers() {
+    
+    private func observeUsers() {
         Api.User.observeUsers { (user) in
             self.users.append(user)
             self.tableView.reloadData()
         }
     }
     
-    func setupTableView(){
+    private func setupTableView(){
         tableView.register(UserCell.self, forCellReuseIdentifier: IDENTIFIER_CELL_USERS)
         
-    }
-    
-    // MARK: - Table view data source
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return users.count
-    }
-    
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: IDENTIFIER_CELL_USERS, for: indexPath) as! UserCell
-        
-        cell.loadData(users[indexPath.row])
-        return cell
-     }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 94
-    }
-    
-    @objc private func requestToggleMenu() {
-        sideMenuDelegate?.shouldToggleMenu()
     }
 }
